d5fccb52d0616c8f2f5e821c6be8863499d812c1
211240047-Xu Yan <211240047@smail.nju.edu.cn>
2025-03-14 15:22:51 +0800

    build the basic framework 3/14

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>Hello, Vite + Vue!</h1>
+</template>
+<!--用来实际配置-，负责定义应用的基本结构和组织其他组件-->
+<script>
+    export default {
+        name: 'App'
+    };
+</script>
+
+<style>
+    h1 {
+        color: blue;
+        text-align: center;
+        margin-top: 20px;
+    }
+</style>
+<style>
+    body {
+        width: 330px;
+        height: 550px;
+        margin: 0;
+        overflow: auto;
+    }
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+//import './style.css'
+import App from './App.vue'
+
+//应用的入口，负责启动和配置应用
+createApp(App).mount('#app')
