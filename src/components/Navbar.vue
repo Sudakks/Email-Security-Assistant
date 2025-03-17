67930d9a5e2280ff9cae4d008558a97fdc423643
211240047-Xu Yan <211240047@smail.nju.edu.cn>
2025-03-17 17:34:01 +0800

    finish navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,51 @@
 <template>
-    <nav>
-        <router-link to="/">Page1</router-link> |
-        <router-link to="/settingPage">Page2</router-link>
+    <nav class="navbar">
+        <router-link to="/" class="nav-item">
+            <img src="../assets/homepage.svg" class="icon" alt="Home" />
+            <span>Threat Overview</span>
+        </router-link>
+        <router-link to="/settingPage" class="nav-item">
+            <img src="../assets/setting.svg" class="icon" alt="Home" />
+            <span>Advanced Options</span>
+        </router-link>
     </nav>
+    <hr class="navBarLine">
 </template>
 
 <style scoped>
-    nav {
-        background: #3498db;
-        padding: 10px;
+    .navbar {
+        display: flex;
+        padding: 0px; /*navbar本身，内部元素如router-link和narbar之间留出空隙*/
+        justify-content: space-evenly; /* 让两个 nav-item 对称分布 */
+        align-items: center;
+        background: white;
+        margin-top:-10px;
+    }
+    .nav-item {
+        display: flex;
+        align-items: center;
+        gap: 5px;
+        background-color: #EEF7FA;
+        padding:5px 10px;
+        border-radius:5px;
+        font-weight:bold;
+        color:black;
+        text-decoration:none;
     }
 
-    a {
-        color: white;
-        text-decoration: none;
-        margin: 0 10px;
+        .nav-item:hover {
+            background-color: #CCECF6;
+        }
+
+    .icon{
+        width:25px;
+        height:25px;
+    }
+    .navBarLine {
+        border: none;
+        color: #EBEBEB;
+        height: 2px;
+        margin: 8px 10px;
+        border-top: 3px solid;
     }
 </style>
